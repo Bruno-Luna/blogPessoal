e64e484ce1e552a777c6c0d35ca148f70253ffa1
Bruno Luna <luna.brsilva@gmail.com>
2021-09-29 22:22:44 -0300

    [Inserção de atributos: foto e tipo e alteração do banco de dados]

---
+++ backend/blogpessoal/src/main/java/com/BlogPessoal/projeto/generation/modelos/utilidades/UsuarioDTO.java
@@ -0,0 +1,79 @@
+package com.BlogPessoal.projeto.generation.modelos.utilidades;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+
+public class UsuarioDTO {
+	
+	@NotBlank @Email 
+    private String email;
+	
+	@NotBlank
+    private String senha;
+
+    private Long id;
+    private String nome;
+    private String token;
+    
+    private String foto;
+    
+    private String tipo;
+    
+    
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
