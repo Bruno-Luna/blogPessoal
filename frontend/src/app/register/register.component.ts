c70ecc372cb697e49f26475f29a67059c88c13b1
Bruno Luna <luna.brsilva@gmail.com>
2021-09-30 21:08:22 -0300

    [Estilização, regras de negócio e algumas pequenas modificações/inserções]

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserLogin } from '../model/UserLogin';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +10,14 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class RegisterComponent implements OnInit {
 
-  userLogin: UserLogin = new UserLogin
+  user: User = new User
   confirmSenha: string
   tipoUsuario: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     window.scroll(0,0)
@@ -24,6 +29,20 @@ export class RegisterComponent implements OnInit {
 
   tipoUser(event: any){
     this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmSenha){
+      alert('A senha está incorreta.')
+    }else{
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
   }
 
 }
