88d6ff692634661e2ae805b5a31d0cfb916e0ec9
Bruno Luna <luna.brsilva@gmail.com>
2021-10-06 18:52:41 -0300

    [Criação da service/post]

--- frontend/src/app/service/theme.service.ts
+++ frontend/src/app/service/theme.service.ts
@@ -11,7 +11,7 @@ export class ThemeService {
 
   constructor(private http: HttpClient) { }
 
-  token ={
+  token = {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
@@ -19,7 +19,21 @@ export class ThemeService {
     return this.http.get<Tema[]>('http://localhost:8080/tema/todos', this.token)
   }
 
+  getByIdTheme(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/tema/id/${id}`, this.token)
+  }  
+
   postTheme(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('http://localhost:8080/tema/salvar', tema, this.token)
   }
+
+  putTheme(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('http://localhost:8080/tema/atualizar', tema, this.token)
+  }
+
+  deleteTheme(id: number){
+    return this.http.delete<Tema>(`http://localhost:8080/tema/deletar/${id}`, this.token)
+  }
+  
+
 }
