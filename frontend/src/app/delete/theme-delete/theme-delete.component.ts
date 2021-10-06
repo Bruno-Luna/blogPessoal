540e0cf06dbe725706a53b55df576c83b2df0265
Bruno Luna <luna.brsilva@gmail.com>
2021-10-06 18:41:37 -0300

    [Inclusão do verbo Delete e tambem das rotas, html, imports e metodos]

--- frontend/src/app/delete/theme-delete/theme-delete.component.ts
+++ frontend/src/app/delete/theme-delete/theme-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tema } from 'src/app/model/Tema';
+import { ThemeService } from 'src/app/service/theme.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-theme-delete',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemeDeleteComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
+  idTema: number
 
-  ngOnInit(): void {
+  constructor(
+    private temaService: ThemeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(){
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idTema = this.route.snapshot.params['id']
+    this.findByIdTema(this.idTema)
   }
 
+  findByIdTema(id: number){
+    this.temaService.getByIdTheme(id).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  deletar(){
+    this.temaService.deleteTheme(this.idTema).subscribe(()=>{
+      alert('Tema excluído com sucesso!!')
+      this.router.navigate(['/theme'])  
+    })
+  }
 }
