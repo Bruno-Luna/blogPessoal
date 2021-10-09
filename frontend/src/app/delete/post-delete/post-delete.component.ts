bf445c453750c34c4dc61f495ca85d93e2283f2d
Bruno Luna <luna.brsilva@gmail.com>
2021-10-09 08:34:16 -0300

    [Inserção das features do verbo DELETE  em Postagem]

--- frontend/src/app/delete/post-delete/post-delete.component.ts
+++ frontend/src/app/delete/post-delete/post-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from 'src/app/model/Postagem';
+import { PostService } from 'src/app/service/post.service';
+import { ThemeService } from 'src/app/service/theme.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-post-delete',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDeleteComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
 
-  ngOnInit(): void {
+  idPost: number
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostService,
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+    if (environment.token == ''){
+      this.router.navigate(['/login'])
+    }
+
+    this.idPost = this.route.snapshot.params['id']
+    this.findByIdPost(this.idPost)
+
   }
 
+  findByIdPost(id: number){
+    this.postagemService.getByIdPost(id).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+    })
+  }
+
+
+  apagar(){
+   this.postagemService.deletePost(this.idPost).subscribe(()=>{
+     alert('Postagem apagada!!')
+     this.router.navigate(['/home'])
+   })
+  }
 }
